61df6a482fa75512007b14878de5ea83dca1eaf0
Vanessa Balderas Martinez <141200434+NessX4@users.noreply.github.com>
2025-04-15 20:29:57 +0000

    pendingStyleP2

--- Udemy/scoreKeeper/app.js
+++ Udemy/scoreKeeper/app.js
@@ -1,8 +1,11 @@
 const p1btn = document.querySelector('#p1btn');
 const p2btn = document.querySelector('#p2btn');
+const resetbtn = document.querySelector('#resetbtn');
 
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+
+const winingScoreInput = document.querySelector('#playto')
 
 let p1score = 0;
 let p2score = 0;
@@ -17,7 +20,7 @@ p1btn.addEventListener('click', function(){
         }
         p1Display.textContent = p1score;
     }    
-})
+});
 
 p2btn.addEventListener('click', function(){
     if(!isGameOver){
@@ -27,4 +30,19 @@ p2btn.addEventListener('click', function(){
         }
         p2Display.textContent = p2score;
     }   
-})
+});
+
+winingScoreInput.addEventListener('change', function(){
+    winingScore = parseInt(this.value);
+    reset();
+});
+
+resetbtn.addEventListener('click', reset);
+
+function reset(){
+    isGameOver = false;
+    p1score = 0;
+    p2score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+};
